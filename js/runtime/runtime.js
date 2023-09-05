c9ded8caf11e326ba25db2377f32d726e327ba07
cctvboy <2329302958@qq.com>
2023-09-05 17:23:20 +0800

    Site updated: 2023-09-05 17:23:20

--- js/runtime/runtime.js
+++ js/runtime/runtime.js
@@ -1,1 +1,17 @@
-var now=new Date;function createtime(){var e=new Date("09/01/2023 00:00:00");now.setTime(now.getTime()+250);var t=(now-e)/1e3/60/60/24,n=Math.floor(t),a=(now-e)/1e3/60/60-24*n,r=Math.floor(a);1==String(r).length&&(r="0"+r);var o=(now-e)/1e3/60-1440*n-60*r,s=Math.floor(o);1==String(s).length&&(s="0"+s);var i=(now-e)/1e3-86400*n-3600*r-60*s,l=Math.round(i);1==String(l).length&&(l="0"+l);let c="";c=r<18&&r>=9?`<span class='textTip'>本站居然运行了 ${n} 天</span><span id='runtime'>\n     ${r} 小时 ${s} 分 ${l} 秒 </span> <i class='fas fa-heartbeat' style='color:red'></i>`:`<span class='textTip'> <br> 本站居然运行了 ${n} 天\n     </span><span id='runtime'> ${r} 小时 ${s} 分 ${l} 秒 </span> <i class='fas fa-heartbeat' style='color:red'></i>`,document.getElementById("workboard")&&(document.getElementById("workboard").innerHTML=c)}setInterval((()=>{createtime()}),250);
+var now = new Date;
+function createtime() {
+    var t = new Date("09/01/2023 00:00:00");
+    now.setTime(now.getTime() + 250);
+    var e = (now - t) / 1e3 / 60 / 60 / 24, a = Math.floor(e), n = (now - t) / 1e3 / 60 / 60 - 24 * a, r = Math.floor(n);
+    1 == String(r).length && (r = "0" + r);
+    var s = (now - t) / 1e3 / 60 - 1440 * a - 60 * r, i = Math.floor(s);
+    1 == String(i).length && (i = "0" + i);
+    var o = (now - t) / 1e3 - 86400 * a - 3600 * r - 60 * i, l = Math.round(o);
+    1 == String(l).length && (l = "0" + l);
+    let g = "";
+    g = r < 18 && r >= 9 ? `<span class='textTip'>本站居然运行了 ${a} 天</span><span id='runtime'>
+     ${r} 小时 ${i} 分 ${l} 秒 </span> <i class='fas fa-heartbeat' style='color:red'></i>`
+        : `<span class='textTip'> <br> 本站居然运行了 ${a} 天
+     </span><span id='runtime'> ${r} 小时 ${i} 分 ${l} 秒 </span> <i class='fas fa-heartbeat' style='color:red'></i>`,
+        document.getElementById("workboard") && (document.getElementById("workboard").innerHTML = g)
+} setInterval((() => { createtime() }), 250);
