58c4e9c717a66ef95ec416560c101eaa9061e515
cctvboy <110763182+cctvboy@users.noreply.github.com>
2023-10-26 13:36:05 +0000

    更新照片墙 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/3eeb.html","posts/e0b8.html","posts/9dcf.html","posts/5475.html","posts/f89a.html","posts/9b0f.html","posts/f2ce.html","posts/d04e.html","posts/66c7.html","posts/f8ce.html"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
+var posts=["posts/e0b8.html","posts/f8ce.html","posts/3eeb.html","posts/e0b8.html","posts/9dcf.html","posts/5475.html","posts/9b0f.html","posts/f2ce.html","posts/d04e.html","posts/66c7.html","posts/f8ce.html"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
